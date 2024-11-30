acb0063969c8045d37470f0ed272251c1aefcba8
Doigonist <doigonist@gmail.com>
2024-11-30 14:08:25 +0200

    made a very cool joke a lot cooler
    This is a description.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 let reward = document.createElement('p');
+let img = document.createElement('img')
 setTimeout(() => {
     reward.textContent = 'Что-то действительно появилось!';
     document.body.appendChild(reward);
+    img.setAttribute('src', 'img/alexander.png');
+    document.appendChild(img);
 }, 10000);
