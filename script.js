536f96aeb8fffaf91cf885c6d7bdd29b85edb20b
Doigonist <doigonist@gmail.com>
2024-11-30 14:59:35 +0200

    updated website

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 'use strict';
-let reward = document.createElement('p');
-let img = document.createElement('img')
-setTimeout(() => {
-    reward.textContent = 'Что-то действительно появилось!';
-    document.body.appendChild(reward);
-    img.setAttribute('src', 'img/alexander.png');
-    document.body.appendChild(img);
-    img.setAttribute('width', 1000);
-    img.setAttribute('height', 500);
-    
-}, 10000);
